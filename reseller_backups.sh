05a17e612c70d290a45ac9ae6bf9d2f92ca5a8f5
hhamilton-eig <48221769+hhamilton-eig@users.noreply.github.com>
2019-08-25 15:58:10 -0700

    Update reseller_backups.sh

--- reseller_backups.sh
+++ reseller_backups.sh
@@ -3,19 +3,6 @@
 # Collects initial reseller info
 read -ep "Reseller username? " reseller
 reseller_home=$(find /home{1..11} -maxdepth 1 -name $reseller 2>/dev/null)
-partition_usage=$(df -h | grep -oP "(\d+)(?=\%\s+\\${reseller_home%/*})")
-
-# Checks that reseller's home partition usage is less than 50%
-if [[ $partition_usage > 70 ]]; then
-    echo -e "\n${reseller_home%/*} is $partition_usage% full.
-Please remove data from this partition before continuing.\n"
-    exit
-fi
-
-# Creates backup dir
-if [[ ! -d $reseller_home/BackupNow ]]; then
-    install -d -m 0755 -o $reseller -g $reseller $reseller_home/BackupNow
-fi
 
 # Array for resold users + backup paths
 declare -A resolds
@@ -24,16 +11,16 @@ declare -A resolds
 find_backups() {
 backup_paths=$(find /backup{1..11}{,.old}/{archived-backups,cpbackup}/{daily,weekly,monthly}/$1 -maxdepth 0 2>/dev/null)
 for backup in $backup_paths; do
-backup=${backup%$1*}
 if [[ -n $backup ]]; then
-resolds["$1"]+="$backup "
+backup_date=$(date -r ${backup}/lastgenerated +%F)
+backup_path=${backup%$1*}
+backup="${backup_path} ${backup_date}"
+resolds["$1"]+="$backup,"
 fi
 done
 }
-
 # Finds backups depending on whether list was provided
 if [[ -z $1 ]]; then
-
 for resold_user in $(awk -F':' -v var="$reseller" '$5 ~ var {if($9){print $9}}' /var/cpanel/accounting.log); do
     find_backups $resold_user
 done
@@ -49,24 +36,14 @@ fi
 # Asks for most recent backup or backup type
 read -ep "Do you want the most recent backup(s)? (yY/nN) " timeline
   if [[ $timeline == [yY] ]]; then
-    for backup in "${resolds["$resold_user"]}"; do
-    date -r ${backup}/${resold_user}/lastgenerated +%F
-
+    for resold_user in "${!resolds[@]}"; do
+     for backup in "${resolds["$resold_user"]}"; do
+echo $backup
+done
+#date -r "${backup}/${resold_user}/lastgenerated" +%F
+  done
 # find most recent backup and archive it
 # Check backup dates
-else
-  read -ep "Backup type? (daily/weekly/monthly) " backup_type
-
-
-
-# Creates tar.gz for each resold user in $reseller_home/BackupNow
-for resold_user in "${!resolds[@]}"; do
-    if [[ -s $reseller_home/BackupNow/$resold_user.tar.gz ]]; then
-        echo "$reseller_home/BackupNow/$resold_user.tar.gz exists and is not empty, skipping.."
-    else
-        tar -C "${resolds["$resold_user"]}" -cf $reseller_home/BackupNow/$resold_user.tar.gz $resold_user
-    fi
-done
-
-# Fixes ownership of archives
-chown -R $reseller. $reseller_home/BackupNow
+#else
+#  read -ep "Backup type? (daily/weekly/monthly) " backup_type
+fi
